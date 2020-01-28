54c1bd9bad6d79a7d9e8cd396ec0bf8bba9145fc
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2020-01-28 09:31:27 -0700

    Move all-call check to gen_id

--- util.py
+++ util.py
@@ -12,8 +12,6 @@ def cp_hash(inpt: str, octets: int, fmt: str = "hex"):
     if fmt == "int":
         d = s.digest(octets)
         output = int.from_bytes(d, sys.byteorder)
-        if output == 255:
-            print(f"ERROR: Cannot use 255 for Group Call \"{inpt}\" unless All Call")
     return output
 
 
@@ -22,6 +20,10 @@ def gen_id(name, data) -> dict:
         data['id'] = cp_hash(name, PRIVATE_CALL_OCTETS, "int")
     elif data['type'] == GROUP_CALL:
         data['id'] = cp_hash(name, GROUP_CALL_OCTETS, "int")
+        if data['id'] == 255:
+            print(f"ERROR: Cannot use 255 for Group Call \"{name}\" unless All Call")
+    elif data['type'] is None:
+        data['id'] = None
     else:
-        print(f"ERROR: unknown type for {data}: {data['type']}")
+        print(f"ERROR: unknown type for {name}: {data['type']}")
     return data
