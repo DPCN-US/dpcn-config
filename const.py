c70ac3b004edcda3d7b4bafbc70c3823275f3097
Philip Rosenberg-Watt <p.rosenberg-watt@cablelabs.com>
2020-01-28 09:31:51 -0700

    Add CONTACT_TYPES const

--- const.py
+++ const.py
@@ -4,8 +4,14 @@ GROUP_CALL = "Group Call"
 GROUP_CALL_OCTETS = 1  # Capacity plus is 1-254. 255 is reserved for All Call
 GROUP_ID_MAX = 254
 
-PRIVATE_ID_MAX = 65535
 PRIVATE_CALL = "Private Call"
 PRIVATE_CALL_OCTETS = 2  # think IPv4 address space (max. 4)
+PRIVATE_ID_MAX = 65535
 
 SYSTEM_DIR = "systems"
+
+CONTACT_TYPES = [
+    GROUP_CALL,
+    PRIVATE_CALL,
+    None
+]
